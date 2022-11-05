8f20de09880bfbcf0be81efd93a4c0807a85bd7e
danyal <syeddanyalhassan@gmail.com>
2022-11-05 23:47:15 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,20 +1,25 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-public class Solution {
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        for(int i = 1; i <= 10; i++){
-            System.out.println(N + " x " + i + " = " + N*i);
+import java.io.*;
+import java.lang.Math;
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int arr[] = new int[n];
+            arr[0]=a+(int)(Math.pow(2,0)*b);
+            for(int j=1;j<n;j++)
+            {
+                arr[j]=arr[j-1]+(int)(Math.pow(2,j)*b);
+            }
+            for(int el:arr)
+                System.out.print(el+" ");
+            System.out.println();
         }
-        scanner.close();
+        in.close();
     }
 }

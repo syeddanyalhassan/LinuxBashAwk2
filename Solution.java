19b05015a58f94755e7952a77074eebad8c6cc18
danyal <syeddanyalhassan@gmail.com>
2022-11-05 23:41:05 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("================================");
-            for(int i=0;i<3;i++)
-            {
-                String s1=sc.next();
-                int x=sc.nextInt();
-                System.out.printf("%-15s%03d%n",s1,x);
-            }
-            System.out.println("================================");
-
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i = 1; i <= 10; i++){
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+        scanner.close();
     }
 }

b653610caedd91b5753d2695d4f43870f102a797
danyal <syeddanyalhassan@gmail.com>
2022-11-05 23:36:02 +0400

    new files added?

--- Solution.java
+++ Solution.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-        scan.close();
-        // Write your code here.
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
     }
 }

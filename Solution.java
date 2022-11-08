ca74464d66d0d9500133dd6b2a08c77d60d19646
danyal <syeddanyalhassan@gmail.com>
2022-11-08 16:13:59 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,35 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-    public static String getSmallestAndLargest(String s, int k) {
-    String smallest = "";
-    String largest = "";
-    smallest = largest = s.substring(0, k);
-
-    for (int i=1; i<s.length()-k+1; i++) {
-        String substr = s.substring(i, i+k);
-        if (smallest.compareTo(substr) > 0)
-            smallest = substr;
-        if (largest.compareTo(substr) < 0)
-            largest = substr;
-    }
-
-    return smallest + "\n" + largest;
-}
+   public static void main(String[] args) {
         
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-         
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int start=0;
+        int end=A.length()-1;
+        String ans="Yes";
+        while(start<=end){
+            if(A.charAt(start)!=A.charAt(end)){
+                ans="No";
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println(ans);
         
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        int k = scan.nextInt();
-        scan.close();
-      
-        System.out.println(getSmallestAndLargest(s, k));
     }
 }
+
+
+

5079f5d1429a2aca2ae7631d7f91d73cee555b35
danyal <syeddanyalhassan@gmail.com>
2022-11-11 21:00:17 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
-import java.util.regex.*;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+     
+    
+    public static final String regularExpression = "^[a-zA-Z]{1,1}[a-zA-Z0-9_]{7,29}$";
+}
 
-public class Solution
-{
-	public static void main(String[] args){
-		  Scanner in = new Scanner(System.in);
-      int testCases = Integer.parseInt(in.nextLine());
-      while(testCases>0){
-         testCases--;
-         String pattern = in.nextLine();
-         try{
-            Pattern.compile(pattern);
-            System.out.println("Valid");
-         }catch(PatternSyntaxException e){
-            System.out.println("Invalid");
-         }
-      }
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
     }
 }

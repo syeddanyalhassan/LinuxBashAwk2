ca576a2d3538c683ffc68141e6a34c9ac5c7f61d
danyal <syeddanyalhassan@gmail.com>
2022-11-14 21:11:40 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,21 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
 
 public class Solution {
-
-    public static void main(String[] args) {
-	   
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a=new int[n];
-        for (int i=0;i<n;i++)
-        {
-           a[i]=scan.nextInt(); 
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] numArray = new String[6][];
+        int largestSum = 0;
+        for(int i=0; i<6; i++){
+            numArray[i]=sc.nextLine().split(" ");
         }
-        scan.close();
-
-        // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=3;j++){
+                int sum = Integer.parseInt(numArray[i][j])+Integer.parseInt(numArray[i][j+1])+Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])+Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])+Integer.parseInt(numArray[i+2][j+2]);
+                if(i==0 && j==0){
+                    largestSum=sum;
+                }else{
+                    if(sum>largestSum){
+                        largestSum=sum;
+                    }
+                }
+            }
         }
+        System.out.println(largestSum);
     }
 }

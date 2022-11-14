945d58276e7e15e6a88bde376cf9b509a13f968e
danyal <syeddanyalhassan@gmail.com>
2022-11-14 21:53:45 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -6,36 +6,24 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static boolean isBalanced(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (int i=0; i<s.length();++i){
-            if (s.charAt(i) == '(') stack.push('(');
-            else if (s.charAt(i) == '{') stack.push('{');
-            else if (s.charAt(i) == '[') stack.push('[');
-            else if (s.charAt(i) == ')') {
-                if (stack.isEmpty()) return false;
-                if (stack.pop() != '(') return false;
-            }
-            else if (s.charAt(i) == '}') {
-                if (stack.isEmpty()) return false;
-                if (stack.pop() != '{') return false;
-            }
-            else if (s.charAt(i) == ']') {
-                if (stack.isEmpty()) return false;
-                if (stack.pop() != '[') return false;
-            }
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
         }
-        return stack.isEmpty();
+
+HashSet<String> pairs = new HashSet<String>(t);
+
+    for(int i = 0; i < t; i++)
+    {
+        pairs.add("(" + pair_left[i] + ", " + pair_right[i] + ")" );
+        System.out.println(pairs.size());        
     }
-    
-    public static void main(String[] args) {
-        Stack<Character> stack = new Stack<Character>();
-        Scanner sc = new Scanner(System.in);
-        String line;
-        while (sc.hasNextLine()){
-            line = sc.nextLine();
-            if (isBalanced(line)) System.out.println("true");
-            else System.out.println("false");
-        }
+   
     }
 }

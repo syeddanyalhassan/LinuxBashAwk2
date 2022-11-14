87d5e2c9ed12b9a9582a04e42caf09f7dd9e3748
danyal <syeddanyalhassan@gmail.com>
2022-11-14 21:22:55 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);

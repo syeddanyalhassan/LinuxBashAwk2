06d5dc90aab77248c947c682cdc5f78511faaa3b
danyal <syeddanyalhassan@gmail.com>
2022-11-14 13:57:28 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,33 +1,23 @@
-import java.math.BigDecimal;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
-class Solution{
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-    public static void main(String []args){
-        //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
-        }
-      	sc.close();
 
-            Arrays.sort(s, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if (s1 == null || s2 == null) {
-                    return 0;
-                }
-                BigDecimal bd1 = new BigDecimal(s1);
-                BigDecimal bd2 = new BigDecimal(s2);
-                return bd2.compareTo(bd1);
-            }
-        });
-        //Output
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(s[i]);
+
+public class JavaPrimalityTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger();
+        in.close();
+        if (n.isProbablePrime(1)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
         }
     }
-
 }

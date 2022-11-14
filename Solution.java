efbb15e3f572ca921c0188fabd95479da52c32cb
danyal <syeddanyalhassan@gmail.com>
2022-11-14 21:17:10 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,34 +1,35 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
+import java.text.*;
+import java.math.*;
 import java.util.regex.*;
 
-
-
 public class Solution {
-   public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[][] numArray = new String[6][];
-        int largestSum = 0;
-        for(int i=0; i<6; i++){
-            numArray[i]=sc.nextLine().split(" ");
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in); 
+        int range = sc.nextInt();        
+        int[] numbers = new int[range];
+        HashMap<Integer, Integer> h = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++)
+        {
+            numbers[i] = sc.nextInt();
         }
-        
-        for(int i=0;i<=3;i++){
-            for(int j=0;j<=3;j++){
-                int sum = Integer.parseInt(numArray[i][j])+Integer.parseInt(numArray[i][j+1])+Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])+Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])+Integer.parseInt(numArray[i+2][j+2]);
-                if(i==0 && j==0){
-                    largestSum=sum;
-                }else{
-                    if(sum>largestSum){
-                        largestSum=sum;
-                    }
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = i; j < numbers.length; j++){
+                int sum = 0;
+                for (int k = i; k <= j; k = k + 1){
+                    sum = sum + numbers[k];
+                }
+                if (sum < 0){
+                    counter = counter + 1;
                 }
             }
+            
         }
-        System.out.println(largestSum);
-    }
+    
+       System.out.println(counter);
+}
 }

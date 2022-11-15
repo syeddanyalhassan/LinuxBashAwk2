ac2fe9cff8b31079b2977dbc22ce95453a1f08c4
danyal <syeddanyalhassan@gmail.com>
2022-11-15 21:36:05 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,38 +1,28 @@
 import java.util.*;
-class Sports{
+import java.io.*;
 
-    String getName(){
-        return "Generic Sports";
-    }
-  
-    void getNumberOfTeamMembers(){
-        System.out.println( "Each team has n players in " + getName() );
-    }
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
 }
 
-class Soccer extends Sports{
-    @Override
-    String getName(){
-        return "Soccer Class";
-    }
-    @Override
-void getNumberOfTeamMembers() {
-    System.out.println("Each team has 11 players in " + getName());
-}
-
-
-    // Write your overridden getNumberOfTeamMembers method here
-
-}
-
-public class Solution{
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
 	
-    public static void main(String []args){
-        Sports c1 = new Sports();
-        Soccer c2 = new Soccer();
-        System.out.println(c1.getName());
-        c1.getNumberOfTeamMembers();
-        System.out.println(c2.getName());
-        c2.getNumberOfTeamMembers();
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me();
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
 	}
 }

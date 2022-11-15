80fe0d8a7933b9a33e9e895d27db958598ebe8e4
danyal <syeddanyalhassan@gmail.com>
2022-11-15 21:30:49 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,43 +1,38 @@
 import java.util.*;
-interface AdvancedArithmetic{
-  int divisor_sum(int n);
-}
+class Sports{
 
-//Write your code here
-class MyCalculator implements AdvancedArithmetic {
-    public int divisor_sum(int n) {
-        if (n <= 1) { return n; }
-
-        int res = n + 1;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                res += i;
-            }
-        }
-
-        return res;
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
     }
 }
 
-class Solution{
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    @Override
+void getNumberOfTeamMembers() {
+    System.out.println("Each team has 11 players in " + getName());
+}
+
+
+    // Write your overridden getNumberOfTeamMembers method here
+
+}
+
+public class Solution{
+	
     public static void main(String []args){
-        MyCalculator my_calculator = new MyCalculator();
-        System.out.print("I implemented: ");
-        ImplementedInterfaceNames(my_calculator);
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.print(my_calculator.divisor_sum(n) + "\n");
-      	sc.close();
-    }
-    /*
-     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
-     */
-    static void ImplementedInterfaceNames(Object o){
-        Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
-            String interfaceName = theInterfaces[i].getName();
-            System.out.println(interfaceName);
-        }
-    }
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
 }
-

f5ae5f2a4b11d62d5a9a5a94d6038e19909a38fb
danyal <syeddanyalhassan@gmail.com>
2022-11-15 19:21:35 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,98 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-/*
- * Create the Student and Priorities classes here.
- */
- import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-
+import java.io.*;
 import java.util.*;
-class Student implements Comparable<Student>{
-    String name = new String();
-    double cgpa;
-    int id;
-    public Student(String name,double cgpa,int id)
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing()
     {
-        this.name = name;
-        this.cgpa = cgpa;
-        this.id = id;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public int compareTo(Student s)
-    {
-        if(cgpa == s.cgpa)
-        {
-            if(name.compareTo(s.name) == 0)
-            {
-                if(id == s.id)
-                    return 0;
-                else if (id > s.id)
-                    return 1;
-                else
-                    return -1;
-            }
-            else
-                return name.compareTo(s.name);
-        }
-        else if(cgpa > s.cgpa)
-            return -1;
-        else
-            return 1;
+        System.out.println("I am singing");
     }
 }
 
-class Priorities{
-    public ArrayList<Student> getStudents(List<String> events)
-    {
-        int n = events.size();
-        PriorityQueue<Student> pq = new PriorityQueue<Student>();
-        for(String i:events)
-        {
-            String[] s = new String[4];
-            s = i.split("\\s");
-            if(s.length>1)
-            {
-                pq.add(new Student(s[1],Double.valueOf(s[2]),Integer.valueOf(s[3])));
-            }
-            else
-            {
-                pq.poll();
-            }
-        }
-        while(pq.size()>1)
-        {
-            System.out.println(pq.poll().name);
-        }
-        return new ArrayList<Student>(pq);
-    }
-}
+public class Solution{
 
+   public static void main(String args[]){
 
-public class Solution {
-    private final static Scanner scan = new Scanner(System.in);
-    private final static Priorities priorities = new Priorities();
-    
-    public static void main(String[] args) {
-        int totalEvents = Integer.parseInt(scan.nextLine());    
-        List<String> events = new ArrayList<>();
-        
-        while (totalEvents-- != 0) {
-            String event = scan.nextLine();
-            events.add(event);
-        }
-        
-        List<Student> students = priorities.getStudents(events);
-        
-        if (students.isEmpty()) {
-            System.out.println("EMPTY");
-        } else {
-            for (Student st: students) {
-                System.out.println(st.getName());
-            }
-        }
-    }
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
 }

08add12e69b7fe9a6fcf9f2bc7d3bbb99283d127
danyal <syeddanyalhassan@gmail.com>
2022-11-15 20:24:10 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,30 +1,31 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-class Arithmetic{
-    public int add(int a, int b){
-        int sum = a + b; 
-        return sum;
-    }
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
 }
-
-class Adder extends Arithmetic{  
-    public int callAdd(int a, int b){
-        return add(a, b);
+class MyBook extends Book {
+    
+    @Override
+    void setTitle(String s){
+        this.title = s;
     }
+    
 }
-public class Solution{
-    public static void main(String []args){
-        // Create a new Adder object
-        Adder a = new Adder();
-        
-        // Print the name of the superclass on a new line
-        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
-        
-        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
-        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
-     }
+//Write MyBook class here
+
+public class Main{
+	
+	public static void main(String []args){
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+		
+	}
 }

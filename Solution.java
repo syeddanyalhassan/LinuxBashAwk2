f8a7c1255d2ce3fc4417d2e2c5dbe3e30e1a9787
danyal <syeddanyalhassan@gmail.com>
2022-11-15 22:07:59 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,26 +1,32 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
+class MyCalculator {
+    public static int power(int n, int p) throws Exception{
+    if(n < 0 || p < 0){
+        throw new Exception ("n or p should not be negative.");
+    }else if(n==0 && p ==0){
+        throw new Exception("n and p should not be zero.");
+    }
+
+     else {
+        return ((int)Math.pow(n,p));
+    }
+}
+}
 
 public class Solution {
-
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-    try {
-      int x = scan.nextInt();
-      int y = scan.nextInt();
-      System.out.println(x / y);
-    } catch(ArithmeticException | InputMismatchException e) {
-      if (e instanceof ArithmeticException) {
-        System.out.println("java.lang.ArithmeticException: / by zero");
-      } else if (e instanceof InputMismatchException){
-        System.out.println("java.util.InputMismatchException");
-      }
-    }
-    scan.close();
-        
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 }

2b125481c39ea3ce3d06b36e590098ce9e969ab7
danyal <syeddanyalhassan@gmail.com>
2022-11-15 18:59:51 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,36 +1,75 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-public class test {
+public class Solution {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        final Deque<Integer> deque = new ArrayDeque<Integer>();
-        final Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        final int n = in.nextInt();
-        final int m = in.nextInt();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int m = scan.nextInt();
 
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            final int num = in.nextInt();
-            deque.addLast(num);
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num).intValue() + 1);
-            } else {
-                map.put(num, 1);
-            }
+    BitSet b1 = new BitSet(n);
+    BitSet b2 = new BitSet(n);
 
-            if (deque.size() == m + 1) {
-                final int key = deque.removeFirst();
-                final int v = map.get(key);
-                if (v == 1) {
-                    map.remove(key);
-                } else {
-                    map.put(key, v - 1);
-                }
-            }
+    for (int i = 0; i < m; i++) {
+      String opcode = scan.next();
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
 
-            final int cnt = map.size();
-            if (cnt > res) { res = cnt; }
-        }
-        System.out.println(res);
+      switch(opcode) {
+        case "AND":
+          if (num1 == 1 && num2 == 2) {
+            b1.and(b2);
+          } else if (num1 == 2 && num2 == 1) {
+            b2.and(b1);
+          } else if (num1 == 1 && num2 == 1) {
+            b1.and(b1);
+          } else if (num1 == 2 && num2 == 2) {
+            b2.and(b2);
+          }
+          break;
+        case "OR":
+          if (num1 == 1 && num2 == 2) {
+            b1.or(b2);
+          } else if (num1 == 2 && num2 == 1) {
+            b2.or(b1);
+          } else if (num1 == 1 && num2 == 1) {
+            b1.or(b1);
+          } else if (num1 == 2 && num2 == 2) {
+            b2.or(b2);
+          }
+          break;
+        case "XOR":
+          if (num1 == 1 && num2 == 2) {
+            b1.xor(b2);
+          } else if (num1 == 2 && num2 == 1) {
+            b2.xor(b1);
+          } else if (num1 == 1 && num2 == 1) {
+            b1.xor(b1);
+          } else if (num1 == 2 && num2 == 2) {
+            b2.xor(b2);
+          }
+          break;
+        case "FLIP":
+          if (num1 == 1) {
+            b1.flip(num2);
+          } else if (num1 == 2) {
+            b2.flip(num2);
+          }
+          break;
+        case "SET":
+          if (num1 == 1) {
+            b1.set(num2);
+          } else if (num1 == 2) {
+            b2.set(num2);
+          }
+          break;
+      }
+      System.out.println(b1.cardinality() + " " + b2.cardinality());
+    }
     }
 }

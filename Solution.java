01bfeabb4980b725406deae711d61512df1d3899
danyal <syeddanyalhassan@gmail.com>
2022-11-17 21:46:37 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,32 +1,67 @@
-import java.util.Scanner;
-class MyCalculator {
-    public static int power(int n, int p) throws Exception{
-    if(n < 0 || p < 0){
-        throw new Exception ("n or p should not be negative.");
-    }else if(n==0 && p ==0){
-        throw new Exception("n and p should not be zero.");
-    }
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-     else {
-        return ((int)Math.pow(n,p));
+
+class Add {
+    public void add(int... intArgs) {
+        int sum = 0;
+        String separator = "";
+        for (int i : intArgs) {
+            sum += i;
+            System.out.print(separator + i);
+            separator = "+";            
+        }
+        System.out.println("=" + sum);
     }
 }
-}
+
 
 public class Solution {
-    public static final MyCalculator my_calculator = new MyCalculator();
-    public static final Scanner in = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        while (in .hasNextInt()) {
-            int n = in .nextInt();
-            int p = in .nextInt();
-            
-            try {
-                System.out.println(my_calculator.power(n, p));
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
-    }
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
 }
+

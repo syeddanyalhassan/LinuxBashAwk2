4ed356396729c7b0c1598a1d210b84b21fa21190
danyal <syeddanyalhassan@gmail.com>
2022-11-19 21:35:56 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,17 +1,56 @@
 import java.io.*;
 import java.util.*;
-import java.security.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+  
+    public static PerformOperation isOdd() {
+        return (int n) -> n % 2 != 0;
+    }
+
+    public static PerformOperation isPalindrome() {
+        return (int n) -> String.valueOf(n).equals((new StringBuilder(String.valueOf(n)).reverse().toString()));
+    }
+    
+    public static PerformOperation isPrime() {
+        return (int n) -> java.math.BigInteger.valueOf(n).isProbablePrime(1);
+    }
+}
+   
 
 public class Solution {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        Scanner input = new Scanner(System.in);
-        MessageDigest m = MessageDigest.getInstance("SHA-256");
-        m.reset();
-        m.update(input.nextLine().getBytes());
-        for (byte i : m.digest()) {
-            System.out.print(String.format("%02x", i));
-        }
-        System.out.println();
-    }
+ public static void main(String[] args) throws IOException {
+  MyMath ob = new MyMath();
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int T = Integer.parseInt(br.readLine());
+  PerformOperation op;
+  boolean ret = false;
+  String ans = null;
+  while (T--> 0) {
+   String s = br.readLine().trim();
+   StringTokenizer st = new StringTokenizer(s);
+   int ch = Integer.parseInt(st.nextToken());
+   int num = Integer.parseInt(st.nextToken());
+   if (ch == 1) {
+    op = ob.isOdd();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "ODD" : "EVEN";
+   } else if (ch == 2) {
+    op = ob.isPrime();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PRIME" : "COMPOSITE";
+   } else if (ch == 3) {
+    op = ob.isPalindrome();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+   }
+   System.out.println(ans);
+  }
+ }
 }

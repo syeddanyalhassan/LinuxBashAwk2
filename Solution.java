4c1727821fa6a94c22c8337113bf111393f2f2f3
danyal <syeddanyalhassan@gmail.com>
2022-11-19 21:24:39 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,25 +1,17 @@
 import java.io.*;
 import java.util.*;
-import java.security.MessageDigest;
-import java.util.Scanner;
+import java.security.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        sc.close();
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(str.getBytes());
-            // return bytesToHex(md.digest
-            byte[] digest = md.digest();
-            for (byte b : digest) {
-                System.out.printf("%02x", b);
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner input = new Scanner(System.in);
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.reset();
+        m.update(input.nextLine().getBytes());
+        for (byte i : m.digest()) {
+            System.out.print(String.format("%02x", i));
         }
+        System.out.println();
     }
 }

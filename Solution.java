8b17214295ebac01d33c725433ef899a90cfb620
danyal <syeddanyalhassan@gmail.com>
2022-11-19 11:12:10 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,61 +1,75 @@
-import java.lang.annotation.*;
-import java.lang.reflect.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@interface FamilyBudget {
-   String userRole() default "GUEST";
-   int budgetLimit() default 0;
-    
+//Complete the classes below
+class Flower {
+     String whatsYourName() {
+        return "I have many names and types.";
+    }
 }
 
-class FamilyMember {
-   @FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
-   public void seniorMember(int budget, int moneySpend) {
-      System.out.println("Senior Member");
-      System.out.println("Spend: " + moneySpend);
-      System.out.println("Budget Left: " + (budget - moneySpend));
-   }
- 
-   @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
-   public void juniorUser(int budget, int moneySpend) {
-      System.out.println("Junior Member");
-      System.out.println("Spend: " + moneySpend);
-      System.out.println("Budget Left: " + (budget - moneySpend));
-   }
+class Jasmine extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Jasmine";
+    }
 }
+
+class Lily extends Flower {
+
+ @Override
+    String whatsYourName() {
+        return "Lily";
+    }
+    }
+class Lotus extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Lotus";
+    }
+}
+class Region {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+
+class WestBengal extends Region {
+    @Override
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+
+class Karnataka extends Region {
+    @Override
+    Lotus yourNationalFlower() {
+        return new Lotus();
+    }
+}
+class AndhraPradesh extends Region {
+    @Override
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
+
 
 public class Solution {
-   public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
-      int testCases = Integer.parseInt(in.nextLine());
-      while (testCases > 0) {
-         String role = in.next();
-         int spend = in.nextInt();
-         try {
-            Class annotatedClass = FamilyMember.class;
-            Method[] methods = annotatedClass.getMethods();
-            for (Method method : methods) {
-               if (method.isAnnotationPresent(FamilyBudget.class)) {
-                  FamilyBudget family = method
-                        .getAnnotation(FamilyBudget.class);
-                  String userRole = family.userRole();
-                  int budgetLimit = family.budgetLimit();
-                  if (userRole.equals(role)) {
-                     if(budgetLimit >= spend){
-                        method.invoke(FamilyMember.class.newInstance(),
-                              budgetLimit, spend);
-                     }else{
-                        System.out.println("Budget Limit Over");
-                     }
-                  }
-               }
-            }
-         } catch (Exception e) {
-            e.printStackTrace();
-         }
-         testCases--;
+  public static void main(String[] args) throws IOException {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      String s = reader.readLine().trim();
+      Region region = null;
+      switch (s) {
+        case "WestBengal":
+          region = new WestBengal();
+          break;
+        case "AndhraPradesh":
+          region = new AndhraPradesh();
+          break;
       }
-   }
+      Flower flower = region.yourNationalFlower();
+      System.out.println(flower.whatsYourName());
+    }
 }

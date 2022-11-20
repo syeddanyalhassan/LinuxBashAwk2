ddecbc660b45f9fdd0927c9a02c71dbb1eb4a933
danyal <syeddanyalhassan@gmail.com>
2022-11-20 09:32:46 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,2 +1,9 @@
-read input
-printf "%.3f\n" `echo "$input" | bc -l`
+sum=0
+read N
+
+for i in $(seq 1 $N); do
+    read number
+    sum=$(( $sum + $number ))
+done
+
+printf "%.3f\n" `echo "$sum / $N" | bc -l`

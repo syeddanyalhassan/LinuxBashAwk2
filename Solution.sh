a4fdd34d26d5f1728a79587c26f91b136acf8c73
danyal <syeddanyalhassan@gmail.com>
2022-11-20 09:29:10 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,11 +1,2 @@
-read X
-read Y
-read Z
-
-if [[ "$X" == "$Y" && "$X" == "$Z" ]]; then
-    echo 'EQUILATERAL'
-elif [[ "$X" == "$Y" || "$X" == "$Z" || "$Y" == "$Z" ]] ; then
-    echo 'ISOSCELES'
-else
-    echo 'SCALENE'
-fi
+read input
+printf "%.3f\n" `echo "$input" | bc -l`

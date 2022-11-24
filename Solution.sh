b6ed64f2ed35e0e55898539e2148bc97f10d7fce
danyal <syeddanyalhassan@gmail.com>
2022-11-24 15:04:47 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,2 +1,2 @@
-#The text file, with lines re-ordered in descending order (numerically).
-sort -n -r
+#Rearrange the rows of the table in descending order of the values for the average temperature in January (i.e, the mean temperature value provided in the second column) in a tab seperated file. 
+sort -k2 -n -r -t$'\t'

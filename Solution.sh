0122d5b6c85c31d9ab43a4e11abba934271975da
danyal <syeddanyalhassan@gmail.com>
2022-11-24 21:42:48 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,2 +1,8 @@
-#Given a text file, display only those lines which are not followed or preceded by identical replications. 
-uniq -u
+#You are given a CSV file where each row contains the name of a city
+#and its state separated by a comma. Your task is to replace the newlines
+#in the file with semicolons as demonstrated in the sample.
+
+#The -s option is supposed to squeeze every repeated occurence of each
+#character into a single occurence and then change said occurrence.
+
+paste -d ';' -s

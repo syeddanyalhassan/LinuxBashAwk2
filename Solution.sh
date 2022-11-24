f8b77a9bddb2a397b20027969c044f8e212ca32a
danyal <syeddanyalhassan@gmail.com>
2022-11-24 21:12:43 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,2 +1,2 @@
-#compare consecutive lines in a case insensitive manner. So, if a line X is followed by case variants, the output should count all of them as the same (but display only the form X in the second column).
-uniq -i -c | cut -c7-
+#Given a text file, display only those lines which are not followed or preceded by identical replications. 
+uniq -u

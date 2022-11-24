936e55e167026ee3e796709cb83ee61fc7a9dcad
danyal <syeddanyalhassan@gmail.com>
2022-11-24 21:51:13 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,10 +1,8 @@
-#The paste command may be used for merging data from one or more files.
-#Some common uses of the command and its various flags and variations
-#have been listed here.
+#Given a CSV file where each row contains the name of a city and its
+#state separated by a comma, your task is to replace the newlines in
+#the file with tabs as demonstrated in the sample.
 
-#You are given a CSV file where each row contains the name of a city and
-#its state separated by a comma. Your task is to restructure the file so
-#that three consecutive rows are folded into one line and are separated
-#by semicolons.
+#squeeze and translate
 
-paste -d ';' - - -
+paste -s
+tr '\n' '\t'

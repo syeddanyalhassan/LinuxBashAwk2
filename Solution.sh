43d4eff7f2921c5e015b515b23223f25ae47a816
danyal <syeddanyalhassan@gmail.com>
2022-11-24 21:49:06 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,8 +1,10 @@
-#You are given a CSV file where each row contains the name of a city
-#and its state separated by a comma. Your task is to replace the newlines
-#in the file with semicolons as demonstrated in the sample.
+#The paste command may be used for merging data from one or more files.
+#Some common uses of the command and its various flags and variations
+#have been listed here.
 
-#The -s option is supposed to squeeze every repeated occurence of each
-#character into a single occurence and then change said occurrence.
+#You are given a CSV file where each row contains the name of a city and
+#its state separated by a comma. Your task is to restructure the file so
+#that three consecutive rows are folded into one line and are separated
+#by semicolons.
 
-paste -d ';' -s
+paste -d ';' - - -

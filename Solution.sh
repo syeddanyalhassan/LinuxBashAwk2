6584be7ad3823383218334e35b22edd149a18b5a
danyal <syeddanyalhassan@gmail.com>
2022-11-24 21:10:19 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,2 +1,2 @@
-#Given a text file, count the number of times each line repeats itself. Only consider consecutive repetitions. Display the space separated count and line, respectively. There shouldn't be any leading or trailing spaces. Please note that the uniq -c command by itself will generate the output in a different format than the one expected here. 
-uniq -c | cut -c7-
+#compare consecutive lines in a case insensitive manner. So, if a line X is followed by case variants, the output should count all of them as the same (but display only the form X in the second column).
+uniq -i -c | cut -c7-

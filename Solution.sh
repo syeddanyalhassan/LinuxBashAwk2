16d552cc87f1a15532ce8644111bb54c35510350
danyal <syeddanyalhassan@gmail.com>
2022-11-25 08:42:00 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,3 +1,3 @@
-#Given a list of countries, each on a new line, your task is to read them into an array and then display the count of elements in that array.
+#You are given a list of countries, each on a new line. Your task is to read them into an array and then transform them in the following way:The first capital letter (if present) in each element of the array should be replaced with a dot ('.'). Then, display the entire array with a space between each country's names.
 arr=($(cat))
-echo ${#arr[@]}
+echo ${arr[@]/[A-Z]/.}

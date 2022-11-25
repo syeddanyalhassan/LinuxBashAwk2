e6db73ec4857dbd963f9aacda90c79bac712ba94
danyal <syeddanyalhassan@gmail.com>
2022-11-25 08:00:14 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,7 +1,8 @@
-#Given a list of countries, each on a new line, your task is to read them into an array and then display the entire array, with a space between each of the countries' names.
-while read line
+declare -a Array=()
+read X
+while [ "" != $X ]
 do
-    arr=(${arr[@]} $line)
+    Array=("${Array[@]}" "$X")
+    read X
 done
-
-echo ${arr[@]}
+echo ${Array[@]:3:5}

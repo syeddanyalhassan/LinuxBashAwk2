be12ec78633e23eae856a3136e291bd638231324
danyal <syeddanyalhassan@gmail.com>
2022-11-25 08:09:20 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,8 +1,3 @@
-declare -a Array=()
-read X
-while [ "" != $X ]
-do
-    Array=("${Array[@]}" "$X")
-    read X
-done
-echo ${Array[@]:3:5}
+# You are given a list of countries, each on a new line. Your task is to read them into an array and then filter out (remove) all the names containing the letter 'a' or 'A'.
+arr=($(cat))
+echo ${arr[@]/*[aA]*/}

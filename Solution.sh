e1104e0f12c47a9ffe629cad5f17e7312b0f4eb3
danyal <syeddanyalhassan@gmail.com>
2022-11-25 08:55:39 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,4 +1,1 @@
-#There are N integers in an array A. All but one integer occur in pairs. Your task is to find the number that occurs only once
-read
-arr=($(cat)) 
-echo "${arr[@]}" | tr ' ' '\n' |sort | uniq -u | tr '\n' ' '
+grep  -iwe "the" -we "that" -we "then" -we "those"

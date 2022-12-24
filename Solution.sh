01ad7b33656ca6e2855b0cc347d4ae2f8051328d
danyal <syeddanyalhassan@gmail.com>
2022-12-24 20:22:34 +0400

    new files added

--- Solution.sh
+++ Solution.sh
@@ -1,6 +1,1 @@
-awk '{total=$2+$3+$4;
-avg=total/3;
-if(avg>=80) grade="A";
-else if(avg>=60 && avg<80) grade ="B";
-else if(avg>=50 && avg<60) grade ="C";
-else grade="FAIL"; print $0,":",grade;}'
+awk 'ORS=NR%2?";":"\n"'
